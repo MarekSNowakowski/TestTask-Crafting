19963cc36d317ed49de537590b82fc9cf66e6730
Marek Nowakowski <marsznow@gmail.com>
2023-05-05 13:12:21 +0200

    Item grabbing with outlining

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -8,7 +8,48 @@ public class Item : MonoBehaviour
 {
     public Transform anchor;
     
-    private void Awake()
+    public Outline Outline;
+    public Collider Collider;
+    public Rigidbody Rigidbody;
+
+    private bool _outlined;
+    private bool _grabbed;
+
+    private void Update()
     {
+        _outlined = false;
+    }
+
+    private void LateUpdate()
+    {
+        if (_outlined || _grabbed)
+        {
+            Outline.enabled = true;
+        }
+        else
+        {
+            Outline.enabled = false;
+        }
+    }
+    
+    public void OnGrabStart()
+    {
+        Rigidbody.useGravity = false;
+        Rigidbody.angularVelocity = Vector3.zero;
+        Rigidbody.velocity = Vector3.zero;
+        Collider.enabled = false;
+        _grabbed = true;
+    }
+
+    public void OnGrabEnd()
+    {
+        Rigidbody.useGravity = true;
+        Collider.enabled = true;
+        _grabbed = false;
+    }
+
+    public void OnAbleToGrab()
+    {
+        _outlined = true;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Transform ItemHoldAnchor;
+    public Transform CameraAnchor;
+    public float GrabDistance = 3f;
+    public float LerpDuration = 5f;
     
     private StarterAssetsInputs _input;
+    private Item _grabbedItem;
+    private float _moveTime;
 
     private void Start()
     {
@@ -14,9 +19,54 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        if (_grabbedItem == null)
+        {
+            HandleItemDetection();
+        }
+        else
+        {
+            HandleGrabbedItem();
+        }
+    }
+
+    private void HandleItemDetection()
+    {
+        if (Physics.Raycast(CameraAnchor.position, CameraAnchor.transform.forward, out RaycastHit raycastHit, GrabDistance))
+        {
+            if (raycastHit.transform.TryGetComponent(out Item item))
+            {
+                item.OnAbleToGrab();
+                
+                if (_input.grab)
+                {
+                    _grabbedItem = item;
+                    _grabbedItem.OnGrabStart();
+                    _moveTime = 0;
+                }
+            }
+        }
+    }
+
+    private void HandleGrabbedItem()
+    {
         if (_input.grab)
         {
-            Debug.Log("Works");
+            if (_moveTime < LerpDuration)
+            {
+                _grabbedItem.transform.position = Vector3.Lerp(_grabbedItem.transform.position, ItemHoldAnchor.position, _moveTime / LerpDuration);
+            }
+            else
+            {
+                _moveTime = 0;
+            }
+
+            _grabbedItem.transform.rotation = ItemHoldAnchor.rotation;
+            _moveTime += Time.deltaTime;
+        }
+        else
+        {
+            _grabbedItem.OnGrabEnd();
+            _grabbedItem = null;
         }
     }
 }
